26917714813cbefeb31be2dcb3775e6b93ee3609
Mahan Zendedel DH <mahanmmi@gmail.com>
2025-03-07 20:40:32 +0400

    feat: add flux for CD integration

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -8,3 +8,17 @@ module "github-oidc" {
   repositories              = ["Mahanmmi/holded-devops-challenge"]
   oidc_role_attach_policies = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"]
 }
+
+module "flux-system-ecr-credentials-sync-role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  role_name = "flux-system-ecr-credentials-sync"
+  role_policy_arns = {
+    "AmazonEC2ContainerRegistryReadOnly" = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  }
+  oidc_providers = {
+    ex = {
+      provider_arn = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["flux-system:ecr-credentials-sync"]
+    }
+  }
+}
--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "github_oidc_role_arn" {
   value = module.github-oidc.oidc_role
 }
+
+output "flux_system_ecr_credentials_sync_role_arn" {
+  value = module.flux-system-ecr-credentials-sync-role.iam_role_arn
+}
